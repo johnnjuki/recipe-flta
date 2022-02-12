04c1b2c627db768615b16f51191735ae4cc308b4
John Njuki <njukijohn2000@gmail.com>
2022-02-12 23:09:13 +0300

    Add the screens to home page

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:recipe/ui/bookmarks_screen.dart';
-import 'package:recipe/ui/groceries_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import 'colors.dart';
+import 'myrecipes/my_recipes_list.dart';
 import 'recipes/recipe_list.dart';
+import 'shopping/shopping_list.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -12,48 +15,98 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int selectedPage = 0;
-  final List pages = [
-    const RecipeList(),
-    const BookmarksScreen(),
-    const GroceriesScreen(),
-  ];
+  int _selectedIndex = 0;
+  List<Widget> pageList = <Widget>[];
+
+  // TODO: Add index key
+
+  @override
+  void initState() {
+    super.initState();
+    pageList.add(const RecipeList());
+    pageList.add(const MyRecipesList());
+    pageList.add(const ShoppingList());
+    // TODO: Call getCurrentIndex
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // TODO: Call saveCurrentIndex
+  }
 
   @override
   Widget build(BuildContext context) {
+    String title;
+    switch (_selectedIndex) {
+      case 0:
+        title = 'Recipes';
+        break;
+      case 1:
+        title = 'Bookmarks';
+        break;
+      case 2:
+        title = 'Groceries';
+        break;
+      default:
+        title = 'Recipes';
+        break;
+    }
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Recipes',
-          style: TextStyle(color: Colors.black),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-      ),
-      body: pages[selectedPage],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedPage,
-        selectedItemColor: Colors.green,
-        onTap: (index) {
-          setState(() {
-            selectedPage = index;
-          });
-        },
-        items: const <BottomNavigationBarItem>[
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant_menu),
+            icon: SvgPicture.asset(
+              'assets/images/icon_recipe.svg',
+              color: _selectedIndex == 0 ? green : Colors.grey,
+              semanticsLabel: 'Recipes',
+            ),
             label: 'Recipes',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.bookmarks),
+            icon: SvgPicture.asset(
+              'assets/images/icon_bookmarks.svg',
+              color: _selectedIndex == 1 ? green : Colors.grey,
+              semanticsLabel: 'Bookmarks',
+            ),
             label: 'Bookmarks',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.list),
+            icon: SvgPicture.asset(
+              'assets/images/icon_shopping_list.svg',
+              color: _selectedIndex == 2 ? green : Colors.grey,
+              semanticsLabel: 'Groceries',
+            ),
             label: 'Groceries',
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: green,
+        onTap: _onItemTapped,
       ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          systemNavigationBarColor: Colors.white,
+          statusBarColor: Colors.white,
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.dark,
+          systemNavigationBarDividerColor: Colors.white,
+          //Navigation bar divider color
+          systemNavigationBarIconBrightness: Brightness.light,
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: IndexedStack(index: _selectedIndex, children: pageList),
     );
   }
 }
